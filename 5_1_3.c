27c0beb408deaa25fa7ed3d2b6d4980d96b26ff3
Bansi <143823838+bansi-vadher@users.noreply.github.com>
2023-09-15 11:30:55 -0700

    ....

---
+++ 5_1_3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	char a;
+	clrscr();
+
+	printf("enter any character:");
+	scanf("%c",&a);
+
+	if(a=='a'||a=='e'||a=='i'||a=='0'||a=='u')
+	{
+	printf("vowel");
+	}
+	else if
+
+	(a=='A'||a=='E'||a=='I'||a=='O'||a=='U')   {
+	 printf("vowel");
+	 }
+
+	else    {
+	printf("console");
+	 }
+
+	getch();
+}
