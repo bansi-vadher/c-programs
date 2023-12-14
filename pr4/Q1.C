6dc53c4fca6a2d91cd3edcc98133262ea52e9bab
bansi-vadher <bansivadher37@gmail.com>
2023-12-14 12:26:11 +0530

    01|12|2023

---
+++ pr4/Q1.C
@@ -0,0 +1,24 @@
+/*
+11 
+22 33
+44 55 66
+77 88 99 110
+121 132 143 154 165
+*/
+#include<stdio.h>
+int main()
+{    
+     int i,j,n=11;
+     for(i=1;i<=5;i++)
+     {
+          for(j=1;j<=i;j++)
+          {
+          printf("%d ",n);
+          n=n+11;
+          }
+      printf("\n");    
+      }
+     
+
+     return 0;
+}
---
+++ pr4/Q4.C
@@ -0,0 +1,29 @@
+/*
+10101
+ 1010
+  101
+   10
+    1
+*/
+#include<stdio.h>
+int main()
+{
+     int j,i,s;
+
+     for(i=5; i>=1; i--)
+     {
+          for(s=5;s>i;s--)
+          {
+             printf(" ");  
+          }
+
+          for(j=1; j<=i; j++)
+          {
+               printf("%d",j%2);
+          }
+            
+         printf("\n");     
+     }
+
+return 0; 
+}
